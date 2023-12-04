2027e99f35ba0e3e40d9a14c32e3c0ac0a33ed0c
thornywhale <whale148000@gmail.com>
2023-12-04 11:01:27 +0200

    Initial commit

---
+++ server/src/seeders/123123123-Bank.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'Banks',
+      [
+        {
+          cardNumber: '4564654564564564',
+          name: 'SquadHelp',
+          expiry: '11/24',
+          cvc: '453',
+          balance: 0,
+        },
+        {
+          cardNumber: '4111111111111111',
+          name: 'yriy',
+          expiry: '09/24',
+          cvc: '505',
+          balance: 5000,
+        },
+      ],
+      {}
+    );
+  },
+};
